287994ab0c5bd2f957e5531078577b627ca2739f
Jeff Renken <renkenjj@hotmail.com>
2014-04-15 16:02:49 -0500

    update models

--- salaries/people/models.py
+++ salaries/people/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+    name_slug = models.SlugField()
+    title = models.CharField(max_length=255)
+    positionNumber = models.CharField(max_length=255)
+    jobClassNumber = models.CharField(max_length=255)
+    term = models.IntegerField()
+    fteStatus = models.FloatField()            
+    salary = models.IntegerField()
+    def get_absolute_url(self):
+        return "/person/%s/" % self.name_slug
+    def __unicode__(self):
+        return self.name
