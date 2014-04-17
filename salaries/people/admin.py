7e7028e99d7219c555c300c766de6d252471a742
Matt Waite <matt.waite@gmail.com>
2014-04-17 15:30:18 -0500

    Adding db, adding loader script and data, adding admin to see the data, altering models to reflect available data.

--- salaries/people/admin.py
+++ salaries/people/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from people.models import Person
+
+admin.site.register(Person)
+
+
--- salaries/people/models.py
+++ salaries/people/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
 class Person(models.Model):
+    sort_id = models.IntegerField()
     name = models.CharField(max_length=255)
     name_slug = models.SlugField()
-    title = models.CharField(max_length=255)
-    positionNumber = models.CharField(max_length=255)
-    jobClassNumber = models.CharField(max_length=255)
-    term = models.IntegerField()
-    fteStatus = models.FloatField()            
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    position_number = models.CharField(max_length=255, blank=True, null=True)
+    job_class_number = models.CharField(max_length=255, blank=True, null=True)
+    term = models.CharField(max_length=5, blank=True, null=True)
+    fte_status = models.FloatField()            
     salary = models.IntegerField()
     def get_absolute_url(self):
         return "/person/%s/" % self.name_slug
