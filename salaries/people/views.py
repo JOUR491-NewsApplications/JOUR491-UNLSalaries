a86fb578b3c1dad9af41dce930a6313be51edc91
Jeff Renken <renkenjj@hotmail.com>
2014-04-17 17:52:05 -0500

    modified views, urls, made a test index page that prints all the names

--- salaries/people/views.py
+++ salaries/people/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from people.models import Person
+	
+    
+def home(request):
+    names = Person.objects.order_by('name')
+    return render(request, 'index.html', {'names': names})
+    
